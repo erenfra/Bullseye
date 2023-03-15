10405453cf5f49ee543dd8ef03316a0b2dc7e9d2
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-03-15 07:39:50 -0500

    Initial commit

---
+++ Bullseye/Models/Game.swift
@@ -0,0 +1,20 @@
+//
+//  Game.swift
+//  Bullseye
+//
+//  Created by Renato Oliveira Fraga on 3/14/23.
+//
+
+import Foundation
+
+struct Game {
+    
+    var target: Int = Int.random(in: 1...100)
+    var score: Int = 0
+    var round: Int = 1
+    
+    func points(sliderValue: Int) -> Int {
+        return 999
+    }
+    
+}
