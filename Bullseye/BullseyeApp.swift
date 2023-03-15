160e0a27bfd77309690755c533876d5fe6ef9095
Renato Oliveira Fraga <rofraga74@gmail.com>
2023-03-14 19:56:34 -0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Renato Oliveira Fraga on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
